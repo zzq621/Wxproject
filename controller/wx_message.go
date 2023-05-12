841822128bf9b23bc27310053bc1cf05dc8a0e15
zhengzeqin <zhengzeqin@valuelinkltd.com>
2023-05-12 10:19:24 +0800

    优化代码结构

--- controller/wx_message.go
+++ controller/wx_message.go
@@ -86,6 +86,8 @@ func TalkWeixin(c *gin.Context) {
 	if err != nil {
 		xlog.Log.Errorf("反序列化数据错误：%v", err)
 	}
+	//提前向微信返回成功接受，防止微信多次回调
+	c.JSON(http.StatusOK, "")
 	accessToken, err := accessToken()
 	if err != nil {
 		xlog.Log.Errorf("获取accesstoken错误：%v", err)
@@ -96,8 +98,6 @@ func TalkWeixin(c *gin.Context) {
 	if err != nil {
 		return
 	}
-	//提前向微信返回成功接受，防止微信多次回调
-	c.JSON(http.StatusOK, "")
 	go handleMsgRet(msgRet)
 }
 
